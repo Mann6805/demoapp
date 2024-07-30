0e3c925507262dbfd051ef8aaeaf9b82fdf4052f
Mannn <mannpatel060805@gmail.com>
2024-07-30 23:20:27 +0530

    yessss

--- lib/screens/simplemap.dart
+++ lib/screens/simplemap.dart
@@ -1,70 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class LocationScreen extends StatefulWidget {
-  @override
-  _LocationScreenState createState() => _LocationScreenState();
-}
+const LatLng current = LatLng(25.1193, 55.3773);
 
-class _LocationScreenState extends State<LocationScreen> {
-  String _locationMessage = "Fetching location...";
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _getCurrentLocation();
-  }
-
-  Future<void> _getCurrentLocation() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Check if location services are enabled
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      setState(() {
-        _locationMessage = 'Location services are disabled.';
-        _isLoading = false;
-      });
-      return;
-    }
-
-    // Check location permissions
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission != LocationPermission.whileInUse &&
-          permission != LocationPermission.always) {
-        setState(() {
-          _locationMessage = 'Location permissions are denied';
-          _isLoading = false;
-        });
-        return;
-      }
-    }
-
-    // Get the current position
-    Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high,
-    );
-
-    setState(() {
-      _locationMessage = 'Latitude: ${position.latitude}, Longitude: ${position.longitude}';
-      _isLoading = false;
-    });
-  }
-
+class LocationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Location Screen'),
-      ),
-      body: Center(
-        child: _isLoading
-            ? CircularProgressIndicator()
-            : Text(_locationMessage),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: current,
+          zoom: 14.0, // Specify zoom level for better visibility
+        ),
       ),
     );
   }
