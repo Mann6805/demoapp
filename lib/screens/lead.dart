d75d634e51e7bd46a189167452656b58ccfbae93
Mannn <mannpatel060805@gmail.com>
2024-08-01 00:50:50 +0530

    Final last 2nd commit

---
+++ lib/screens/lead.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class Lead extends StatelessWidget {
+  final String name;
+  final String description;
+  final String slot;
+  final String landmark;
+  final double distance;
+
+  const Lead({
+    Key? key,
+    required this.name,
+    required this.description,
+    required this.slot,
+    required this.landmark,
+    required this.distance,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+          border: Border.all(color: Color(0XFF693907), width: 2),
+          borderRadius: BorderRadius.circular(10)),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        elevation: 5,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              child: Image.asset(
+                'assets/images/papervendor.png',
+                width: 400,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              " $name",
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              '  Scrap Description: $description',
+              style: const TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              '  Slot: $slot',
+              style: const TextStyle(fontSize: 14),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              '  Landmark: $landmark',
+              style: const TextStyle(fontSize: 14),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              '  $distance km away',
+              style: const TextStyle(fontSize: 14),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey,
+                  ),
+                  child: const Text(
+                    'Reject',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0XFF693907),
+                  ),
+                  child: const Text(
+                    'Accept',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20),
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/vendorhomescreen.dart
+++ lib/screens/vendorhomescreen.dart
@@ -1,11 +1,12 @@
+import 'dart:convert';
+
+import 'package:demoapp/screens/lead.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
-import 'package:getwidget/getwidget.dart';
 import 'package:http/http.dart' as http;
+import 'package:getwidget/getwidget.dart';
+import 'dart:async';
 
 class VendorHomeScreen extends StatefulWidget {
-
   String vendorid;
   VendorHomeScreen({super.key, required this.vendorid});
 
@@ -14,205 +15,248 @@ class VendorHomeScreen extends StatefulWidget {
 }
 
 class _VendorHomeScreenState extends State<VendorHomeScreen> {
-
   bool leads = true;
   bool online = false;
+  List<Map<String, dynamic>> pickupRequests = [];
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _startPolling();
+  }
+
+  void _startPolling() {
+    _timer = Timer.periodic(const Duration(seconds: 3), (timer) {
+      fetchPickupRequests().then((requests) {
+        setState(() {
+          pickupRequests = requests;
+        });
+      }).catchError((error) {
+        print('Failed to load pickup requests: $error');
+      });
+    });
+  }
+
+  Future<List<Map<String, dynamic>>> fetchPickupRequests() async {
+    final url = Uri.parse(
+        'https://trashandler-api-s-1-259j.onrender.com/pickup-request/');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = jsonDecode(response.body);
+      return data.cast<Map<String, dynamic>>();
+    } else {
+      throw Exception('Failed to load pickup requests');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-
     final double _deviceWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Image(image: const AssetImage("assets/images/logo.png"), width: _deviceWidth/3,),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Image(
+              image: const AssetImage("assets/images/logo.png"),
+              width: _deviceWidth / 3,
+            ),
+          ),
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(bottom: Radius.circular(25))),
+          backgroundColor: const Color(0XFF693907),
         ),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(25)
-          )
-        ),
-        backgroundColor: const Color(0XFF693907),
-        
-      ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              InkWell(
-                onTap: () {
-                  leads = true;
-                  setState(() {});
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: leads ? const Color(0XFF693907) : const Color(0XFF9C9898), 
-                    borderRadius: const BorderRadius.all(Radius.circular(12))
-                  ),
-                  width: _deviceWidth/2.3,
-                  
-                  child: const Center(
-                    child: Padding(
-                      padding: EdgeInsets.only(top: 10, right: 20, bottom: 10, left: 20),
-                      child: 
-                        Text("New Leads",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14
-                          ),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  onTap: () {
+                    leads = true;
+                    setState(() {});
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: leads
+                            ? const Color(0XFF693907)
+                            : const Color(0XFF9C9898),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(12))),
+                    width: _deviceWidth / 2.3,
+                    child: const Center(
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            top: 10, right: 20, bottom: 10, left: 20),
+                        child: Text(
+                          "New Leads",
+                          style: TextStyle(color: Colors.white, fontSize: 14),
                         ),
-                    ),
-                  ),
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  leads = false;
-                  setState(() {});
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: leads ? const Color(0XFF9C9898) : const Color(0XFF693907), 
-                    borderRadius: const BorderRadius.all(Radius.circular(12))
-                  ),
-                  width: _deviceWidth/2.3,
-                  
-                  child: const Center(
-                    child: Padding(
-                      padding: EdgeInsets.only(top: 10, right: 20, bottom: 10, left: 20),
-                      child: 
-                        Text("Accepted Leads",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14
-                          ),
-                        ),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          leads ? Padding(
-            padding: const EdgeInsets.only(right: 21,left: 21),
-            child: Container(
-              height: 45,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(15)),
-                border: Border.all(
-                  color: const Color(0XFF693907)
-                ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(left: 20.0),
-                    child: Text(
-                      "You are online", 
-                      style: TextStyle(
-                        fontSize: 15
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 20.0),
-                    child: GFToggle(
-                        onChanged: (online) async {
-                          print(online);
-                          online! ? online = true : online = false;
-                          final url = Uri.parse("https://trashandler-api-s-1-259j.onrender.com/vendor_status/");
-
-                          var request = http.MultipartRequest('POST', url);
-
-                          request.fields['vendor_id'] = widget.vendorid;
-                          request.fields['is_active'] = online.toString();
-
-                          final response = await request.send();
-                          print("${await response.stream.bytesToString()}");
-                        },
-                        value: online,
-                        enabledThumbColor:Colors.white,
-                        enabledTrackColor: const Color(0XFF48AC38),
-                        type: GFToggleType.ios,
+                ),
+                InkWell(
+                  onTap: () {
+                    leads = false;
+                    setState(() {});
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: leads
+                            ? const Color(0XFF9C9898)
+                            : const Color(0XFF693907),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(12))),
+                    width: _deviceWidth / 2.3,
+                    child: const Center(
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            top: 10, right: 20, bottom: 10, left: 20),
+                        child: Text(
+                          "Accepted Leads",
+                          style: TextStyle(color: Colors.white, fontSize: 14),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            leads
+                ? Padding(
+                    padding: const EdgeInsets.only(right: 21, left: 21),
+                    child: Container(
+                      height: 45,
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(15)),
+                        border: Border.all(color: const Color(0XFF693907)),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(left: 20.0),
+                            child: Text(
+                              "You are online",
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 20.0),
+                            child: GFToggle(
+                              onChanged: (value) => {
+                                online = value!,
+                                setState(() {}),
+                              },
+                              value: online,
+                              enabledThumbColor: Colors.white,
+                              enabledTrackColor: const Color(0XFF48AC38),
+                              type: GFToggleType.ios,
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   )
-                ],
-              ),
+                : const SizedBox(
+                    height: 10,
+                  ),
+            const SizedBox(
+              height: 10,
             ),
-          )
-          : const SizedBox( height: 10,),
-          const SizedBox(
-            height: 50,
-          ),
-          leads ? Column(
-            children: online ? [] : [
-              Image(image: const AssetImage("assets/images/vendornewlead.png"), width: _deviceWidth/1.1,),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text("No scrap leads to show !", style: TextStyle(fontSize: 20,)),
-              SizedBox(
-                width: _deviceWidth/1.6,
-                child: const Text("More opportunities are on the way! Check back soon for the Leads", 
-                    style: TextStyle(fontWeight: FontWeight.w300, fontSize: 17),
-                    textAlign: TextAlign.center,
-                  ),
-              ),
-            ],
-          ): Column(
-            children: [
-              Image(image: const AssetImage("assets/images/vendoracceptlead.png"), width: _deviceWidth/1.1,),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text("No accepted leads yet !", style: TextStyle(fontSize: 20,)),
-              SizedBox(
-                width: _deviceWidth/1.6,
-                child: const Text("keep the momentum going! New scrap opportunities are waiting. Review the 'New Leads' section to find your next haul", 
-                    style: TextStyle(fontWeight: FontWeight.w300, fontSize: 17),
-                    textAlign: TextAlign.center,
-                  ),
-              ),
-            ],
-          )
-          ,
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: const Color(0XFF693907),
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.grey,
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home"
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.location_on_outlined),
-            label: "Track"
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history_outlined),
-            label: "History"
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline),
-            label: "Account"
-          )
-        ],
-      )
-    );
+            online
+                ? Expanded(
+                    child: ListView.builder(
+                      itemCount: pickupRequests.length,
+                      itemBuilder: (context, index) {
+                        var lead = pickupRequests[index];
+                        return Lead(
+                            name: lead['customer_id'],
+                            description: lead['description'],
+                            slot: lead['timeslots'],
+                            landmark: lead['landmark'],
+                            distance: 3);
+                      },
+                    ),
+                  )
+                : leads
+                    ? Column(
+                        children: [
+                          Image(
+                            image: const AssetImage(
+                                "assets/images/vendornewlead.png"),
+                            width: _deviceWidth / 1.1,
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Text("No scrap leads to show !",
+                              style: TextStyle(
+                                fontSize: 20,
+                              )),
+                          SizedBox(
+                            width: _deviceWidth / 1.6,
+                            child: const Text(
+                              "More opportunities are on the way! Check back soon for the Leads",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w300, fontSize: 17),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      )
+                    : Column(
+                        children: [
+                          Image(
+                            image: const AssetImage(
+                                "assets/images/vendoracceptlead.png"),
+                            width: _deviceWidth / 1.1,
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Text("No accepted leads yet !",
+                              style: TextStyle(
+                                fontSize: 20,
+                              )),
+                          SizedBox(
+                            width: _deviceWidth / 1.6,
+                            child: const Text(
+                              "keep the momentum going! New scrap opportunities are waiting. Review the 'New Leads' section to find your next haul",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w300, fontSize: 17),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: const Color(0XFF693907),
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.location_on_outlined), label: "Track"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.history_outlined), label: "History"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline), label: "Account")
+          ],
+        ));
   }
 }
